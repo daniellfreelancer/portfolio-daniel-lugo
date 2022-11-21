55a05efec4f7e6da7730b81f84e7ab752203fd14
Daniel Lugo <daniellugofreelance@gmail.com>
2022-11-21 20:58:57 -0300

    create master to deploy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,13 +9,13 @@ export default function Footer() {
             </div>
             <div className='footer-div'>
                 <a href="http://drive.google.com/uc?export=view&id=1eRjG9zSzfqzohOdbP75AoVqQOc5mpTwR" target="_blank" download="Daniel-Lugo-FullStack-CV">
-                    <img className="link-footer" src='http://drive.google.com/uc?export=view&id=1uuhFohETxC9mRNhXxlVwPTPy4QNd6IGj' alt='cv' />
+                    <img className="link-footer" src='http://drive.google.com/uc?export=view&id=1uuhFohETxC9mRNhXxlVwPTPy4QNd6IGj' alt='cv logo' />
                 </a>
                 <a href="https://github.com/daniellfreelancer" target="_blank">
-                    <img className="link-footer" src='http://drive.google.com/uc?export=view&id=1KpFtZ0xmYHCKc-Wj-7Mbfshj5RaKMNvI' alt='github' />
+                    <img className="link-footer" src='http://drive.google.com/uc?export=view&id=1KpFtZ0xmYHCKc-Wj-7Mbfshj5RaKMNvI' alt='github logo' />
                 </a>
                 <a href="https://www.linkedin.com/in/daniel-lugo-roberty/" target="_blank">
-                    <img className="link-footer" src='http://drive.google.com/uc?export=view&id=1na_cYe-RLcaZ2KM7mYpxjuxzmd2neDrl' alt='linkedin' />
+                    <img className="link-footer" src='http://drive.google.com/uc?export=view&id=1na_cYe-RLcaZ2KM7mYpxjuxzmd2neDrl' alt='linkedin logo' />
                 </a>
             </div>
         </>

eae4df01733d888270a89ece852ec12d050f4632
Daniel Lugo <daniellugofreelance@gmail.com>
2022-12-14 11:00:14 -0300

    ready for PWA service worker

--- src/App.js
+++ src/App.js
@@ -12,12 +12,56 @@ import VideoWelcome from "./components/VideoWelcome";
 import Mobile from "./components/Mobile";
 import ScrollToTop from "./components/ScrollToTop";
 import Footer from "./components/Footer";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isReadyForInstall, setIsReadyForInstall] = useState(false)
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (event) => {
+      // Prevent the mini-infobar from appearing on mobile.
+     event.preventDefault();
+      console.log("👍", "beforeinstallprompt", event);
+      // Stash the event so it can be triggered later.
+      window.deferredPrompt = event;
+      // Remove the 'hidden' class from the install button container.
+      setIsReadyForInstall(true);
+    });
+  }, []);
+
+  async function downloadApp() {
+    console.log("👍", "butInstall-clicked");
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+      // The deferred prompt isn't available.
+      console.log("oops, no prompt event guardado en window");
+      return;
+    }
+    // Show the install prompt.
+    promptEvent.prompt();
+    // Log the result
+    const result = await promptEvent.userChoice;
+    console.log("👍", "userChoice", result);
+    // Reset the deferred prompt variable, since
+    // prompt() can only be called once.
+    window.deferredPrompt = null;
+    // Hide the install button.
+    setIsReadyForInstall(false);
+  }
+
+  if(setIsReadyForInstall) {
+    downloadApp();
+  }
+  
 
   return (
     <div className="App">
+
       <NavBar />
+      {/* <div className="container">
+        <button className="btn btn-warning">Descargar</button>
+
+      </div> */}
       <VideoWelcome />
       <Tech />
       <main>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,3 +16,16 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+serviceWorkerRegistration.register({
+  onUpdate: async (registration) => {
+    // Corremos este código si hay una nueva versión de nuestra app
+    // Detalles en: https://developers.google.com/web/fundamentals/primers/service-workers/lifecycle
+    if (registration && registration.waiting) {
+      await registration.unregister();
+      registration.waiting.postMessage({ type: "SKIP_WAITING" });
+      // Des-registramos el SW para recargar la página y obtener la nueva versión. Lo cual permite que el navegador descargue lo nuevo y que invalida la cache que tenía previamente.
+      window.location.reload();
+    }
+  },
+});
